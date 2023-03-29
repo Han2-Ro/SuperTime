59dac6a2a6fe240f4b1df8f67585c10b3760030f
Han2-Ro <hannes.rother@online.de>
2023-03-29 18:29:30 +0200

    Added MainActivity with bottom bar navigation to switch between Alarms, Timers and Settings

--- app/src/main/java/com/han2dev/supertime_v0/MainActivity.kt
+++ app/src/main/java/com/han2dev/supertime_v0/MainActivity.kt
@@ -1,73 +1,35 @@
 package com.han2dev.supertime_v0
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Button
-import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
-import com.google.gson.Gson
-
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.han2dev.supertime_v0.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: TimesRecViewAdapter
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        recyclerView = findViewById(R.id.rootRecView)
-        adapter = TimesRecViewAdapter(this, recyclerView)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        val navView: BottomNavigationView = binding.navView
 
-        val btnStart: Button = findViewById(R.id.btnStart)
-        btnStart.setOnClickListener {
-            val intent = Intent(this, TimerActivity::class.java)
-            intent.putExtra("timer", adapter.updateTimer())
-            startActivity(intent)
-        }
-
-        //TODO: consider removing these add buttons
-        val btnAdd: Button = findViewById(R.id.btnAddTime)
-        btnAdd.setOnClickListener {
-            adapter.add(TimerElem())
-        }
-
-        val btnAddLoop: Button = findViewById(R.id.btnAddLoop)
-        btnAddLoop.setOnClickListener {
-            adapter.add(TimerLoop(1))
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.timer_setup_menu,  menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.optSave -> save()
-            R.id.optAddTimer -> adapter.add(TimerElem())
-            R.id.optAddLoop -> adapter.add(TimerLoop())
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    private fun save() {
-        //val json1 = Json.encodeToString(adapter.timer)
-        adapter.updateTimer()
-        val gson = Gson()
-        val json = gson.toJson(adapter.timer)
-        println(json)
-        Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show()
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_alarm, R.id.navigation_timer, R.id.navigation_settings
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/han2dev/supertime_v0/TimerSetupActivity.kt
@@ -0,0 +1,71 @@
+package com.han2dev.supertime_v0
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+
+
+class TimerSetupActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: TimesRecViewAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timer_setup)
+
+        recyclerView = findViewById(R.id.rootRecView)
+        adapter = TimesRecViewAdapter(this, recyclerView)
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val btnStart: Button = findViewById(R.id.btnStart)
+        btnStart.setOnClickListener {
+            val intent = Intent(this, TimerActivity::class.java)
+            intent.putExtra("timer", adapter.updateTimer())
+            startActivity(intent)
+        }
+
+        //TODO: consider removing these add buttons
+        val btnAdd: Button = findViewById(R.id.btnAddTime)
+        btnAdd.setOnClickListener {
+            adapter.add(TimerElem())
+        }
+
+        val btnAddLoop: Button = findViewById(R.id.btnAddLoop)
+        btnAddLoop.setOnClickListener {
+            adapter.add(TimerLoop(1))
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.timer_setup_menu,  menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.optSave -> save()
+            R.id.optAddTimer -> adapter.add(TimerElem())
+            R.id.optAddLoop -> adapter.add(TimerLoop())
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun save() {
+        //val json1 = Json.encodeToString(adapter.timer)
+        adapter.updateTimer()
+        val gson = Gson()
+        val json = gson.toJson(adapter.timer)
+        println(json)
+        Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show()
+    }
+}
