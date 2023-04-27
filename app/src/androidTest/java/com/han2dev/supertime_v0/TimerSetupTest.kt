bca33acb485657b1f57352d6bf8ae1a31832e96b
Han2-Ro <hannes.rother@online.de>
2023-04-28 00:16:59 +0200

    Tried testing composables

--- app/src/androidTest/java/com/han2dev/supertime_v0/TimerSetupTest.kt
+++ app/src/androidTest/java/com/han2dev/supertime_v0/TimerSetupTest.kt
@@ -1,23 +1,31 @@
 package com.han2dev.supertime_v0
 
+import android.content.Intent
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.createEmptyComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextClearance
 import androidx.compose.ui.test.performTextInput
 import org.junit.Rule
 import org.junit.Test
 
 class TimerSetupTest {
 	@get:Rule
-	val rule = createComposeRule()
+	val rule = createComposeRule()//createAndroidComposeRule(NewTimerSetupActivity::class.java)
 
 	@Test
-	fun test() {
+	fun test_textFieldInput() {
 		val timerLoop = TimerLoop()
-		//timerLoop.childrenTimers.add(TimerElem(5000))
+		timerLoop.childrenTimers.add(TimerElem(5000))
 		rule.setContent { TimeLI(timer = TimerElem()) }
+		Thread.sleep(1000)
+		rule.onNodeWithTag("minutesField").performTextClearance()
+		rule.onNodeWithTag("secondsField").performTextClearance()
 		rule.onNodeWithTag("minutesField").performTextInput("1")
 		rule.onNodeWithTag("secondsField").performTextInput("30")
+		Thread.sleep(1000)
 		rule.onNodeWithText(" min  ").assertExists()
 		rule.onNodeWithText(" sec").assertExists()
 		rule.onNodeWithText("1").assertExists()
